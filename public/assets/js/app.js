8b0ac17fb0c85bd3ad5eb1103676637180704f76
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2021-06-02 17:23:35 +0545

    footer

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,26 @@
 $('#fullname').on('focus',function(){
     $('#name-div').addClass('border-logo text-logo')
+    $('#name-logo').addClass('text-logo')
 })
 $('#fullname').on('focusout', function() {
     $('#name-div').removeClass('border-logo text-logo')
+    $('#name-logo').removeClass('text-logo')
+})
+
+$('#mail').on('focus',function(){
+    $('#mail-div').addClass('border-logo text-logo')
+    $('#mail-logo').addClass('text-logo')
+})
+$('#mail').on('focusout', function() {
+    $('#mail-div').removeClass('border-logo text-logo')
+    $('#mail-logo').removeClass('text-logo')
+})
+
+$('#subject').on('focus',function(){
+    $('#subject-div').addClass('border-logo text-logo')
+    $('#subject-logo').addClass('text-logo')
+})
+$('#subject').on('focusout', function() {
+    $('#subject-div').removeClass('border-logo text-logo')
+    $('#subject-logo').removeClass('text-logo')
 })
