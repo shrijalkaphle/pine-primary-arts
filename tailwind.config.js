d9472a071b00522bf0be78578fd3047f6ddd443d
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2021-06-02 15:51:38 +0545

    file upload

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      logo: '#00B6AB',
+      black: {
+        DEFAULT: colors.black,
+        light: '#171515'
+      },
+      white: colors.white
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
+}
